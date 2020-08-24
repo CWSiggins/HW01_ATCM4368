7278aa5182e5e52bbe95e92f0bcb3e8fbb05dc06
Cameron Siggins <sigginscameron@gmail.com>
2020-08-24 18:09:09 -0500

    Added All Unity Files

---
+++ SigginsCameron_HW01/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BallMotor))]
+public class Player : MonoBehaviour
+{
+    //TODO offload health into its own script (Health.cs)
+    [SerializeField] int _maxHealth = 3;
+    int _currentHealth;
+
+    BallMotor _ballMotor;
+
+    private void Awake()
+    {
+        _ballMotor = GetComponent<BallMotor>();
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessMovement();  
+    }
+
+    private void ProcessMovement()
+    {
+        //TODO move into Input script
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical).normalized;
+
+        _ballMotor.Move(movement);
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth += amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        Debug.Log("Player's health: " + _currentHealth);
+
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Player's health: " + _currentHealth);
+        if(_currentHealth <= 0)
+        {
+            Kill();
+        }
+
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        // TODO add particles
+        // TODO play sound
+    }
+}
